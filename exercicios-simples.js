e5854fd3c77af1708c5c57c318444d24766d7397
Gabriel Rodrigues <gabriel_rodrigues_perez@hotmail.com>
2020-01-15 19:43:13 -0300

    ultimo commit

--- exercicios-simples.js
+++ exercicios-simples.js
@@ -1,5 +1,5 @@
  //Javascript
-        humano = {
+        var humano = {
             nome:"Gabriel Rodrigues",
             peso:110,
             idade:21,
@@ -7,8 +7,8 @@
             trabalhando:false
         };
 
-        console.log(humano);
-
+        
+       
         // operações matemáticas com javascript
 
         /* 
@@ -31,3 +31,41 @@
         // conta normal 
         a = 10, y =5;
         console.log(a+y);
+
+
+        // desafio 
+
+        var endereco = {
+            rua:"Rua dos pinheiros",
+            numero:1293,
+            bairro:"centro",
+            cidade: "São Paulo",
+            uf:"SP"
+        };
+
+        var e = endereco;
+
+        console.log("o usuario mora em "+ e.cidade +"/"+e.uf+ ", no bairro "+ e.bairro+", na "+e.rua+", no n°: "+e.numero);
+
+        function pares(x,y){
+           
+                for(x; x < y;x++){
+                    if(x % 2 == 0)
+                         console.log(x);
+                
+                }
+        }
+
+        pares(32,321)
+
+       var skill = ["javascript","NodeJs","ReactJS","React-native"];
+       element = "js";
+        function haveSkl(skill){
+            element = skill.indexOf(element);
+            if(element === skill.indexOf(element))
+                console.log(true);
+            else
+                console.log(false);
+
+        }
+        haveSkl(element);
